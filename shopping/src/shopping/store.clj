fbc0cf10c798b825f81068c302935e453e62f4d9
akthrms <akt.hrms@gmail.com>
2021-08-24 02:10:00 +0900

    update

--- shopping/src/shopping/store.clj
+++ shopping/src/shopping/store.clj
@@ -29,3 +29,28 @@
   "stock an item on the shelves"
   [item]
   (swap! inventory update-in [item] inc))
+
+(declare sold-items)
+
+(defn restock-order
+  "a watch to restock an item"
+  [k v ov nv]
+  (doseq [item (for [kw (keys ov)
+                     :when (not= (kw ov) (kw nv))]
+                 kw)]
+    (swap! sold-items update-in [item] (fnil inc 0))
+    (println "need to restock item")))
+
+(defn init-with-restock
+  "set up store with inventory"
+  [m]
+  (def inventory (atom m))
+  (def sold-items (atom {}))
+  (set-validator! inventory no-negative-values?)
+  (add-watch inventory :restock restock-order))
+
+(defn restock-all
+  "restock all items sold"
+  []
+  (swap! inventory #(merge-with + % @sold-items))
+  (reset! sold-items {}))

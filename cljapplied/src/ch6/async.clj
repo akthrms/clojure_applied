87a611284b28c36680327e6a93014a6e5416f59a
akthrms <akt.hrms@gmail.com>
2021-08-26 02:22:17 +0900

    update

--- cljapplied/src/ch6/async.clj
+++ cljapplied/src/ch6/async.clj
@@ -1,43 +1,47 @@
 (ns ch6.async
-  (:require [clojure.core.async :refer [admix
-                                        chan
-                                        dropping-buffer
-                                        mix
-                                        mult
-                                        pipe
-                                        pub
-                                        sub
-                                        tap
-                                        toggle]]))
+  (:require [clojure.core.async :as async]))
 
-(defn make-comp-1 [])
+(defn make-component-1 [])
 
-(defn make-comp-2 [])
+(defn make-component-2 [])
 
-(defn get-output [comp1])
+(defn get-output [component])
 
-(defn get-input [comp2])
+(defn get-input [component])
 
-(let [component1 (make-comp-1)
-      input-chan (get-output component1)
-      component2 (make-comp-2)
-      output-chan (get-input component2)]
-  (pipe input-chan output-chan))
+(let [component-1 (make-component-1)
+      input (get-input component-1)
+      component-2 (make-component-2)
+      output (get-output component-2)]
+  (async/pipe input output))
 
 (defn connect-and-tap
   "Connect input and output and return channel logging data flowing between them."
   [input output]
-  (let [m (mult input)
-        log (chan (dropping-buffer 100))]
-    (tap m output)
-    (tap m log)
+  (let [m (async/mult input)
+        log (async/chan (async/dropping-buffer 100))]
+    (async/tap m output)
+    (async/tap m log)
     log))
 
-(defn assemble-chans []
-  (let [in (chan 10)
-        p (pub in :topic)
-        news-ch (chan 10)
-        weather-ch (chan 10)]
-    (sub p :new news-ch)
-    (sub p :weather weather-ch)
-    [in news-ch weather-ch]))
+(defn assemble-channels []
+  (let [in (async/chan 10)
+        p (async/pub in :topic)
+        news-channel (async/chan 10)
+        weather-channel (async/chan 10)]
+    (async/sub p :new news-channel)
+    (async/sub p :weather weather-channel)
+    [in news-channel weather-channel]))
+
+(defn combine-channels
+  [twitter-channel facebook-channel]
+  (async/merge [twitter-channel facebook-channel] 100))
+
+(defn mix-channels
+  [twitter-channel facebook-channel out]
+  (let [m (async/mix out)]
+    (async/admix m twitter-channel)
+    (async/admix m facebook-channel)
+    (async/toggle m {twitter-channel  {:mute true}
+                     facebook-channel {:mute true}})
+    m))

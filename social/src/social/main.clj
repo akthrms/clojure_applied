97f2ff95c1a195a4a614817dfecf7d3ad6f99c63
akthrms <akt.hrms@gmail.com>
2021-08-28 00:32:14 +0900

    update

---
+++ social/src/social/main.clj
@@ -0,0 +1,25 @@
+(ns social.main
+  (:require [clojure.core.async :as async]
+            [com.stuartsierra.component :as component]
+            [social.components.approvals :as approvals]
+            [social.components.feed :as feed]
+            [social.components.kengine :as kengine]))
+
+(defn system
+  [{:keys [twitter facebook knowledge approvals] :as config}]
+  (let [twitter-chan (async/chan 100)
+        twitter-response-chan (async/chan 10)
+        facebook-chan (async/chan 100)
+        facebook-response-chan (async/chan 10)
+        alert-chan (async/chan 100)
+        response-chan (async/chan 100)
+        feed-chan (async/merge [twitter-chan facebook-chan])
+        response-pub (async/pub response-chan :feed)]
+    (async/sub response-pub :twitter twitter-response-chan)
+    (async/sub response-pub :facebook facebook-response-chan)
+    (component/system-map
+      :twitter (feed/new-feed twitter twitter-chan twitter-response-chan)
+      :facebook (feed/new-feed facebook facebook-chan facebook-response-chan)
+      :knowledge-engine (kengine/new-knowledge-engine knowledge feed-chan alert-chan)
+      :approvals (component/using (approvals/new-approvals approvals alert-chan response-chan)
+                                  [:knowledge-engine]))))

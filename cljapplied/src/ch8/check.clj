0db1a1e6bd3236146878a3edc23a3c064b96b820
akthrms <akt.hrms@gmail.com>
2021-08-31 01:02:42 +0900

    update

--- cljapplied/src/ch8/check.clj
+++ cljapplied/src/ch8/check.clj
@@ -43,3 +43,44 @@
             (gen/hash-map :name gen-food
                           :quantity gen/nat
                           :unit gen-unit)))
+
+(def identity-conversion-prop
+  (prop/for-all [u gen-unit
+                 n gen/nat]
+                (= n (convert u u n))))
+
+(def conversion-order-prop
+  (prop/for-all [u1 gen-unit
+                 u2 gen-unit
+                 u3 gen-unit
+                 u4 gen-unit
+                 n gen/nat]
+                (= (->> n
+                        (convert u1 u2)
+                        (convert u2 u3)
+                        (convert u3 u4))
+                   (->> n
+                        (convert u1 u3)
+                        (convert u3 u2)
+                        (convert u2 u4)))))
+
+(def roundtrip-conversion-prop
+  (prop/for-all [u1 gen-unit
+                 u2 gen-unit
+                 q gen/nat]
+                (and (= q
+                        (convert u1 u2 (convert u2 u1 q))
+                        (convert u2 u1 (convert u1 u2 q))))))
+
+(defn add-and-convert
+  [i1 i2 i3 output-unit]
+  (let [{:keys [quantity unit]} (ingredient+ i1 (ingredient+ i2 i3))]
+    (convert unit output-unit quantity)))
+
+(def associative-ingredient+-prop
+  (prop/for-all [i1 gen-ingredient
+                 i2 gen-ingredient
+                 i3 gen-ingredient]
+                (= (add-and-convert i1 i2 i3 (:unit i1))
+                   (add-and-convert i3 i1 i2 (:unit i1))
+                   (add-and-convert i2 i1 i3 (:unit i1)))))
